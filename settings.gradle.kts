facc562cb7ddd03acefd33330fdfc7309fedeb3f
gauthamas <gauthamas@gmail.com>
2025-06-11 13:47:52 +0530

    Initial module structure

---
+++ settings.gradle.kts
@@ -0,0 +1,27 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "netty"
+include(":app")
+include(":netty-core")
+include(":network-monitor")
+include(":sync-engine")
+include(":conflict-resolver")
